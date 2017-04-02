99c4c60526d58edf4e75dd9e492e016ff1ce2b9b
kevin <929118967@qq.com>
2017-04-02 15:22:29 +0800

    Initial Commit

---
+++ KNGestureSideslipRemovalController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNGestureSideslipRemovalController
+//
+//  Created by devzkn on 02/04/2017.
+//  Copyright © 2017 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
